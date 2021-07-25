e1882521ff18198f5b514d9d288e8f4ab1b8873c
harithtech <harith.trmz@gmail.com>
2021-07-25 20:04:17 +0800

    Second Commit

---
+++ lib/views/home.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Icon(Icons.restaurant_menu),
+            SizedBox(width: 10),
+            Text('Food Recipe'),
+          ],
+        ),
+      ),
+    );
+  }
+}
